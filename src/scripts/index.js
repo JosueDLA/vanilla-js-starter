b3674374eaa4bc5655c6e4029aa6b34f0a88205c
JosueDLA <josuedlavs@gmail.com>
2021-06-09 23:13:53 -0600

    Add Route functionality

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,10 @@
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { PATHS } from "./routes";
+import Router from "./router";
 import "../styles/index.css";
+
+const ROUTER = new Router(PATHS);
 
 if (process.env.NODE_ENV === "development") {
   require("../index.html");
